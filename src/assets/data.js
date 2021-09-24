4475688a3835c54b14d43616e19b379aba5c94e4
Jean Alexander Woldner <alexander@woldner.io>
2021-09-24 13:12:44 +0200

    :sparkles: add test data

---
+++ src/assets/data.js
@@ -0,0 +1,40 @@
+const data = [
+  {
+    id: 1,
+    name: "Aquaman",
+    description:
+      "Halvmänniskan och halvatlanten Arthur Curry får veta att han är arvinge till undervattensriket Atlantis och måste träda fram för att leda sitt folk och bli en hjälte. Där börjar hans livs resa - en resa som inte bara tvingar honom att möta sitt sanna jag, utan också upptäcka om han är värdig det han föddes till att bli: en kung. ",
+    image:
+      "https://iptvimagecache-playplus-prod-vip.han.telia.se/crop/200x290/http%3A%2F%2Fiptvlogin.telia.se%2F%2FSF%20Anytime%2F4749441.jpg",
+    video: "http://mirrors.standaloneinstaller.com/video-sample/dolbycanyon.mp4",
+  },
+  {
+    id: 2,
+    name: "Incoming",
+    description:
+      "En terroristgrupp skickas till den internationella rymdstationen, som numera fungerar som ett topphemligt fängelse. När gruppens ledare lyckas fly från sin cell, tar han hjälp av de andra fångarna för att ta över rymdstationen och använda den som en missil med Moskva som mål. Nu kan bara en man stoppa dem.",
+    image:
+      "https://iptvimagecache-playplus-prod-vip.han.telia.se/crop/200x290/http%3A%2F%2Fiptvlogin.telia.se%2F%2FSF%20Anytime%2F4810859.jpg",
+    video: "http://mirrors.standaloneinstaller.com/video-sample/P6090053.mp4",
+  },
+  {
+    id: 3,
+    name: "Sune",
+    description:
+      "När Sune ska börja fyran möts han av en chock. En ny kille sitter på hans plats, och han är allt som Sune vill vara. Inte nog med det, han heter Sune också, och det är ju verkligen ingen bra start på fyran. Sune måste använda all sin list för att inte Sophie ska lämna honom och välja den nye Sune. Samtidigt känner Håkan att hans storebror håller på att sugas upp i de vuxnas tråkiga värld, pappa Rudolf får en ålderskris och vill säga upp sig för att satsa på musiken och mamma Karin blänger avundsjukt på nya Sunes perfekta mamma och hennes ännu mer perfekta bil. Välkommen till familjen Andersson!",
+    image:
+      "https://iptvimagecache-playplus-prod-vip.han.telia.se/crop/200x290/http%3A%2F%2Fiptvlogin.telia.se%2F%2Ftimbuktu%2F4608171.jpg",
+    video: "http://mirrors.standaloneinstaller.com/video-sample/small.mp4",
+  },
+  {
+    id: 4,
+    name: "Voxlux",
+    description:
+      "Efter att nästan ha mist livet i en skolskjutning framför den 14-åriga Celeste en sång vid en minnesstund för offren och blir stjärna över en natt. Nästan två decennier senare är Celeste en sliten bråkmakare vars karriär nästan havererat efter en skandal. Hon gör sig redo för sin comeback med skivan Vox Lux, men vill USA fortfarande ha sin lilla älskling?",
+    image:
+      "https://iptvimagecache-playplus-prod-vip.han.telia.se/crop/200x290/http%3A%2F%2Fiptvlogin.telia.se%2F%2FSF%20Anytime%2F4810857.jpg",
+    video: "http://mirrors.standaloneinstaller.com/video-sample/TRA3106.mp4",
+  },
+]
+
+export default data
