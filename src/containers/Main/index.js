72a14242403e45e084d7d614fe0bd179108bf5b5
Jean Alexander Woldner <alexander@woldner.io>
2021-09-24 13:17:39 +0200

    :bug: fix broken import

--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { Wrapper, Container, Section } from "./styled"
 import Player from "../../components/Player"
 import List from "../../components/List"
-import data from "../../constants/data"
+import data from "../../assets/data"
 
 const Main = () => {
   const [url, setUrl] = useState(data[0].video)
