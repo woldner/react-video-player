31a16d18c2206291bd5025c2b41c14dfe50730a4
Jean Alexander Woldner <alexander@woldner.io>
2021-09-24 13:15:14 +0200

    :sparkles: add Main container

---
+++ src/containers/Main/index.js
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import { Wrapper, Container, Section } from "./styled"
+import Player from "../../components/Player"
+import List from "../../components/List"
+import data from "../../constants/data"
+
+const Main = () => {
+  const [url, setUrl] = useState(data[0].video)
+
+  const handleClick = (url) => {
+    setUrl(url)
+  }
+
+  return (
+    <Wrapper>
+      <Container>
+        <Section>
+          <Player url={url} />
+        </Section>
+
+        <Section>
+          <List items={data} clickHandler={handleClick} />
+        </Section>
+      </Container>
+    </Wrapper>
+  )
+}
+
+export default Main
---
+++ src/containers/Main/styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+export const Wrapper = styled.main`
+  background-color: #282c34;
+  padding: 16px;
+  text-align: center;
+`
+
+export const Container = styled.div`
+  align-items: center;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  font-size: calc(10px + 2vmin);
+  justify-content: center;
+  min-height: 100vh;
+`
+
+export const Section = styled.section`
+  max-width: 1200px;
+  width: 100%;
+`
