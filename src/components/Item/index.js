735d0aae134f08c638cd140aa2a2b799269f88c1
Jean Alexander Woldner <alexander@woldner.io>
2021-09-29 15:36:40 +0200

    :sparkles: don't use closures

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -2,9 +2,17 @@ import Tooltip from "react-float-tooltip"
 import { Button, Poster, TooltipContainer, Title } from "./styled"
 
 const Item = ({ data, clickHandler }) => {
+  const createTooltip = () => {
+    return <TooltipContainer>{data.description}</TooltipContainer>
+  }
+
+  const handleClick = () => {
+    clickHandler(data.video)
+  }
+
   return (
-    <Tooltip tooltipElement={() => <TooltipContainer>{data.description}</TooltipContainer>}>
-      <Button onClick={() => clickHandler(data.video)}>
+    <Tooltip tooltipElement={createTooltip}>
+      <Button onClick={handleClick}>
         <Poster src={data.image} alt={data.name} />
         <Title>{data.name}</Title>
       </Button>
